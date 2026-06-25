23d4778134b8cc4695ecf927151d44c97fd440e0
Thanawat Chantakhin <Bright23650@gmail.com>
2026-06-25 15:40:27 +0700

    feat: add hello world program

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("สวัสดีครับ ผม (ธนวัฒน์ จันทะขิน)\n");
+    printf("รหัสนักศึกษา: [692110144]\n");
+    printf("วิชา : DG111 Computer Programming\n");
+    return 0;
+}
